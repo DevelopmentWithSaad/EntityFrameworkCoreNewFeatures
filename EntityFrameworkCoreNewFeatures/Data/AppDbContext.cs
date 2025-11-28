7e1c1cd54f4c88cccad9872653103cf21112e259
SaadAslam <SaadAslam@PMLaptop>
2025-11-28 07:09:12 +0500

    Category table Temporal and Global query filter

--- EntityFrameworkCoreNewFeatures/Data/AppDbContext.cs
+++ EntityFrameworkCoreNewFeatures/Data/AppDbContext.cs
@@ -16,6 +16,31 @@ namespace EntityFrameworkCoreNewFeatures.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            //Category
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasKey(e=>e.Id);
+                entity.Property(e => e.Name)
+                .IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Description)
+                .HasMaxLength(500);
+                //Make Category table Temporal, this way history table creates automatically
+                entity.ToTable("Categories", b => b.IsTemporal(temporal =>
+                {
+                    temporal.HasPeriodStart("PeriodStart");
+                    temporal.HasPeriodEnd("PeriodEnd");
+                    temporal.UseHistoryTable("CategoriesHistory");
+                }));
+                //Global query filter filter soft delete records
+                entity.HasQueryFilter(c => !c.IsDeleted);
+                //initial seed data for categories table 
+                entity.HasData(
+                    new Category { Id=1, Name="Electronics", Description="Electronic Devices", CreatedDate=DateTime.Now, ModifiedDate=DateTime.Now},
+                    new Category { Id=2, Name="Books", Description="Story Books", CreatedDate=DateTime.Now, ModifiedDate=DateTime.Now},
+                    new Category { Id=3, Name="Clothing", Description="Winter Clothes", CreatedDate=DateTime.Now, ModifiedDate=DateTime.Now}
+                    );
+            });
+
 
         }
     }
